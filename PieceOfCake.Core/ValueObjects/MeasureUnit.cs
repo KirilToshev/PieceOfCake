2de579e54c6b4e5f8e4190b66720db336ca56bf8
Kir4o <kiko.toshev@gmail.com>
2019-12-15 20:28:08 +0200

    Api and Core projects

---
+++ PieceOfCake.Core/ValueObjects/MeasureUnit.cs
@@ -0,0 +1,23 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+
+namespace PieceOfCake.Core.ValueObjects
+{
+    public class MeasureUnit
+    {
+        private MeasureUnit(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public static Result <MeasureUnit> Create(string? name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return Result.Fail<MeasureUnit>("Error");
+             
+            return Result.Ok(new MeasureUnit(name));
+        }
+    }
+}
