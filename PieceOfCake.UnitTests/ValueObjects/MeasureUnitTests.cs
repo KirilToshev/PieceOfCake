5edf395d9f15ac20c66aa5bc09ada7a934a0d88e
Kiril Toshev <kiko.toshev@gmail.com>
2020-01-17 15:50:01 +0200

    Unit test adding and fixing the others

--- PieceOfCake.UnitTests/ValueObjects/MeasureUnitTests.cs
+++ PieceOfCake.UnitTests/ValueObjects/MeasureUnitTests.cs
@@ -1,3 +1,4 @@
+using AutoFixture;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -14,24 +15,31 @@ namespace PieceOfCake.UnitTests.ValueObjects
     public class MeasureUnitTests
     {
         private IResources _resources;
-        
+        private Mock<IUnitOfWork> _uowMock;
+        private Mock<IMeasureUnitRepository> _measureUnitRepoMock;
+        private Fixture _fixture; 
+
         [SetUp]
         public void BeforeEachTest()
         {
+            _fixture = new Fixture();
             IServiceCollection services = new ServiceCollection();
             services.AddResources();
             var serviceProvider = services.BuildServiceProvider();
             _resources = serviceProvider.GetService<IResources>();
-            var moq = new Mock<IMeasureUnitRepository>()
+            _uowMock = new Mock<IUnitOfWork>();
+            _measureUnitRepoMock = new Mock<IMeasureUnitRepository>();
+            _uowMock.Setup(x => x.MeasureUnitRepository)
+                .Returns(_measureUnitRepoMock.Object);
+            _measureUnitRepoMock
                 .Setup(x => x.GetFirstOrDefault(It.IsAny<Expression<Func<MeasureUnit, bool>>>()))
-                .Returns(x => )
-                
+                .Returns((MeasureUnit)null);
         }
 
         [Test]
         public void Should_Return_User_Error_If_Created_Without_Name()
         {
-            var measureUnit = MeasureUnit.Create("", _resources);
+            var measureUnit = MeasureUnit.Create("", _resources, _uowMock.Object);
             Assert.IsTrue(measureUnit.IsFailure);
             Assert.AreEqual("Measure Unit must have name.", measureUnit.Error);
         }
@@ -39,17 +47,27 @@ namespace PieceOfCake.UnitTests.ValueObjects
         [Test]
         public void Should_Return_User_Error_If_Name_Exceeds_Symbols_Count_Limit()
         {
-            var measureUnit = MeasureUnit.Create(new string('|', 51), _resources);
+            var measureUnit = MeasureUnit.Create(new string('|', 51), _resources, _uowMock.Object);
             Assert.IsTrue(measureUnit.IsFailure);
             Assert.AreEqual("Measure Unit name should not exceed 50 symbols.", measureUnit.Error);
         }
 
         [Test]
-        public void Should_Return_User_Error_If_Name_Exceeds_Symbols_Count_Limit()
+        public void Should_Return_User_Error_If_Name_Already_Exists()
         {
-            var measureUnit = MeasureUnit.Create(new string('|', 51), _resources);
+            //Arrange
+            var alreadyExistingName = _fixture.Create<string>();
+            var mesureUnit = MeasureUnit.Create(alreadyExistingName, _resources, _uowMock.Object).Value;
+            _measureUnitRepoMock
+                .Setup(x => x.GetFirstOrDefault(It.IsAny<Expression<Func<MeasureUnit, bool>>>()))
+                .Returns(mesureUnit);
+
+            //Act
+            var measureUnit = MeasureUnit.Create(alreadyExistingName, _resources, _uowMock.Object);
+
+            //Assert
             Assert.IsTrue(measureUnit.IsFailure);
-            Assert.AreEqual("Measure Unit name should not exceed 50 symbols.", measureUnit.Error);
+            Assert.AreEqual(string.Format("An entity with name {0} already exist.", alreadyExistingName), measureUnit.Error);
         }
     }
 }
